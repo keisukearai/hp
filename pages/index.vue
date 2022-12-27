13b3c3dcd59335819f6bccc25d1649043cf87bd5
keisukearai <araiautocom@gmail.com>
2022-12-27 16:17:17 +0900

    add svg

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="page_body">
-        <h3>トップページ</h3>
+        <div class="flex justify-center">
+            <a href="https://undraw.co/illustrations" target="_blank">
+                <img src="/image/undraw_services_re_hu5n.svg" />
+            </a>
+        </div>
+        <div class="flex justify-center">
+            <img src="/image/undraw_working_remotely_re_6b3a.svg" />
+        </div>
     </div>
 </template>
 
