5a6e432e4dded30a98730f2e7340d032d6c32eb0
keisukearai <araiautocom@gmail.com>
2023-02-02 10:34:38 +0900

    layout change

--- pages/company.vue
+++ pages/company.vue
@@ -9,76 +9,163 @@
                     <div class="h-1 w-20 bg-indigo-500 rounded"></div>
                 </div>
                 <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">
-                    企業や組織のためにカスタムソフトウェアやアプリケーションの開発のお手伝いを行っております。
-                    複数のシステムを統合し、一元的な管理ができるようにするアドバイス、改善提案を提供します。
-                    データ管理のニーズに合わせて、データベースシステムの構築や運用を行います。
+                    Mission：ITシステムを通じて世界をよりよくする。
+                    Vision：お客様のご要望に答え続ける。
+                    Value：スピード感を大切にしてお客様に寄り添う。
                 </p>
             </div>
-            <div class="flex flex-wrap">
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        会社名
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.name }}
-                    </p>
+            <div class="flex flex-wrap -m-4">
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">会社名</h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.name }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        代表者
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.representative }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                    <circle cx="12" cy="7" r="4"></circle>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    代表者
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.representative }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        設立
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.established }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    設立
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.established }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        所在地
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        <p>{{ company.zip }}</p>
-                        <p>{{ company.address }}</p>
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    所在地
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            <p>{{ company.zip }}</p>
+                            <p>{{ company.address }}</p>
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        事業内容
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4 whitespace-pre-line">
-                        {{ company.business_content }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    事業内容
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base whitespace-pre-line">
+                            {{ company.business_content }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        資本金
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.capital }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 7.5l3 4.5m0 0l3-4.5M12 12v5.25M15 12H9m6 3H9m12-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    資本金
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.capital }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        取引銀行
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.business_bank }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    取引銀行
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.business_bank }}
+                        </p>
+                    </div>
                 </div>
-                <div class="xl:w-1/4 lg:w-1/2 md:w-full mb-10 px-8 py-6 border-l-2 border-gray-200 border-opacity-60">
-                    <h2 class="text-lg sm:text-xl text-gray-900 font-medium title-font mb-2">
-                        決算期
-                    </h2>
-                    <p class="leading-relaxed text-base mb-4">
-                        {{ company.accounting_period }}
-                    </p>
+                <div class="xl:w-1/3 md:w-1/2 p-4">
+                    <div class="border border-gray-200 p-6 rounded-lg">
+                        <div class="flex">
+                            <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"></path>
+                                </svg>
+                            </div>
+                            <div class="inline-flex pt-2 mx-5">
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">
+                                    決算期
+                                </h2>
+                            </div>
+                        </div>
+                        <p class="leading-relaxed text-base">
+                            {{ company.accounting_period }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
--- pages/newsdetail/_id.vue
+++ pages/newsdetail/_id.vue
@@ -18,7 +18,7 @@
                             <div>{{ $dayjs(newsdetail.updatedAt).format('YYYY/MM/DD') }}</div>
                             <div class="text-lg">{{ newsdetail.title }}</div>
                         <br />
-                        <div class="whitespace-pre-line">{{ newsdetail.content }}</div>
+                        <div v-html="newsdetail.content"></div>
                         <div class="flex justify-end">
                             <a href='/news' class="inline-block px-4 py-1.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-700 hover:shadow-lg focus:bg-gray-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true">
                                 back
