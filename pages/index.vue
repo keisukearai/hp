4edcb29bef173f7a777615dc9f8b1f7bcde96be4
keisukearai <araiautocom@gmail.com>
2023-02-08 13:07:03 +0900

    pagespeed

--- pages/index.vue
+++ pages/index.vue
@@ -18,9 +18,9 @@
                 <div class="xl:w-1/4 md:w-1/2 p-4">
                     <div class="bg-gray-100 p-6 rounded-lg shadow-md">
                         <img class="h-40 rounded w-full object-cover object-center mb-6" src="/image/undraw_programming_re_kg9v.svg" alt="content">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <div class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</div>
                         <h2 class="text-lg text-gray-900 font-medium title-font mb-4">受託開発</h2>
-                        <p class="leading-relaxed text-base">
+                        <p class="leading-relaxed text-base text-gray-700">
                             Webアプリケーション開発をメインに開発を行っております。
                             サーバーサイド言語は、Java、PHP、Python。フロントサイド言語は、HTML、CSS、JQuery、Vueとなります。
                             Linuxでサーバ構築を行います。
@@ -30,9 +30,9 @@
                 <div class="xl:w-1/4 md:w-1/2 p-4">
                     <div class="bg-gray-100 p-6 shadow-md rounded-lg">
                         <img class="h-40 rounded w-full object-cover object-center mb-6" src="/image/undraw_informed_decision_p2lh.svg" alt="content">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <div class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</div>
                         <h2 class="text-lg text-gray-900 font-medium title-font mb-4">ITコンサルティング</h2>
-                        <p class="leading-relaxed text-base">
+                        <p class="leading-relaxed text-base text-gray-700">
                             社内ITシステムに関するアドバイスや改善提案を行っております。
                             顧客のビジネス上の要件と技術的な要件をバランス良く満たす提案をモットーにしております。
                         </p>
@@ -41,9 +41,9 @@
                 <div class="xl:w-1/4 md:w-1/2 p-4">
                     <div class="bg-gray-100 p-6 shadow-md rounded-lg">
                         <img class="h-40 rounded w-full object-cover object-center mb-6" src="/image/undraw_educator_re_ju47.svg" alt="content">
-                        <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
+                        <div class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</div>
                         <h2 class="text-lg text-gray-900 font-medium title-font mb-4">プログラミング教育</h2>
-                        <p class="leading-relaxed text-base">
+                        <p class="leading-relaxed text-base text-gray-700">
                             オンラインでのマンツーマンレッスンを行っております。開発未経験者にも丁寧に指導させて頂きます。
                         </p>
                     </div>
